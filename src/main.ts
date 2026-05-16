acebb773e4bbc95ac9a622ea95705cd01205cd09
DJsexum <juancruzcabrerasocin@gmail.com>
2026-05-15 22:45:59 -0300

    No se que hice

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
+async function bootstrap() 
+{
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors('*');
 
   app.useGlobalPipes
     (new ValidationPipe
